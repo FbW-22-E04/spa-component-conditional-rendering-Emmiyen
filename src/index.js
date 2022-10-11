7175e2ba78e0573ba4d114dbc97d628a20d0c052
Danica Yener <ny21037@googlemail.com>
2022-10-11 18:04:51 +0200

    Component-conditional

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import ReactDOM from "react-dom";
 const Alert = () => {
   const colorClasses = {
     color: "red",
-	text4Red:"OMG! Something really bad has happended!",
+	text4Red:"OMG! Something really bad has happened!",
 	text4Orange:"Well, it is not that bad after all!"
   };
   //your component here
-
+  return (
+    <div class={`alert ${colorClasses.color==="red"?"alert-danger":"alert-warning"}`} role="alert">
+     { colorClasses.color==="red"?colorClasses.text4Red:colorClasses.text4Orange}
+    </div>
+  );
 };
 
 // here is where the alert component is being used, you don't have to edit this part,
